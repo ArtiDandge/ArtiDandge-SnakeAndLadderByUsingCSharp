5a28b729401f47b0932285268a99ac7b74590c94
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-11-30 22:30:23 +0530

    [Arti Dandge] Add : Player Roll dice to get number between 1 to 6

--- SnakeAndLadderProject/Program.cs
+++ SnakeAndLadderProject/Program.cs
@@ -4,10 +4,18 @@ namespace SnakeAndLadderProject
 {
     class Program
     {
+        // Function to roll a dice and get number between 1-6
+        public static int rollDice()
+        {
+            Random random = new Random();
+            int dicevalue = random.Next(0,7);
+            return dicevalue;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder Project !");
             int PlayerPosition = 0;
+            int PlayerDice = rollDice();
         }
     }
 }
