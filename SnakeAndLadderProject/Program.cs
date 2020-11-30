1fff1c391077f7d29cf3471bb61564c42bb496e4
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-11-30 22:43:00 +0530

    [Arti Dandge] Add : Player check for options. They are No Play, Ladder or Snake

--- SnakeAndLadderProject/Program.cs
+++ SnakeAndLadderProject/Program.cs
@@ -8,14 +8,39 @@ namespace SnakeAndLadderProject
         public static int rollDice()
         {
             Random random = new Random();
-            int dicevalue = random.Next(0,7);
+            int dicevalue = random.Next(1,7);
             return dicevalue;
+        }
+
+        //Function to check Player get a Ladder or snake or player play or not
+        public static int checkOption()
+        {
+            Random random = new Random();
+
+            return random.Next(1, 4);
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder Project !");
             int PlayerPosition = 0;
             int PlayerDice = rollDice();
+            int option = checkOption();
+            // Use Switch case for option
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Players not play");
+                    break;
+                case 2:
+                    Console.WriteLine("Player got the Ladder");
+                    break;
+                case 3:
+                    Console.WriteLine("Player got the Snake");
+                    break;
+                default:
+                    Console.WriteLine("Something Went Wrong");
+                    break;
+            }
         }
     }
 }
